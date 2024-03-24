3b81e65c2f8ac68f19c84fee7a99583881c337e2
Marko Siladin <marko@mxs42.com>
2024-03-24 14:09:01 -0400

    block with background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import * as THREE from 'three';
+	import * as SC from 'svelte-cubed';
+
+	let width = 1;
+	let height = 1;
+	let depth = 1;
+</script>
+
+<SC.Canvas antialias background={new THREE.Color('papayawhip')}>
+	<SC.Mesh
+		geometry={new THREE.BoxGeometry()}
+		material={new THREE.MeshStandardMaterial({ color: 0xff3e00 })}
+	/>
+	<SC.PerspectiveCamera position={[1, 1, 3]} />
+        <SC.OrbitControls enableZoom={false} />
+	<SC.AmbientLight intensity={0.6} />
+	<SC.DirectionalLight intensity={0.6} position={[-2,3,2]} />
+</SC.Canvas>
